23ffac9eb4904f50016f0c4977e1755739daf5c1
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-04-26 04:26:20 +0900

    Initial Commit

---
+++ ScrollViewSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollViewSample
+//
+//  Created by 真有 津坂 on 12/04/26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ScrollViewSample/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ScrollViewSample
+//
+//  Created by 真有 津坂 on 12/04/26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
