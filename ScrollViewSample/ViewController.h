35f81a47cf6d647e01fe88afd5c63cab3e209f71
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-04-26 05:13:43 +0900

    Add Comment.

--- ScrollViewSample/ViewController.h
+++ ScrollViewSample/ViewController.h
@@ -8,6 +8,25 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    //以下追加
+    BOOL keyboardShown;
+    CGPoint offset;
+}
+
+@property (weak, nonatomic) IBOutlet UIScrollView *myscrollView;
+
+@property (weak, nonatomic) IBOutlet UITextField *myField;
+
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *myLabel2;
+
+@property (weak, nonatomic) IBOutlet UITextField *myField2;
+
+
+- (IBAction)keyHide:(id)sender;
+
+- (IBAction)keyHide2:(id)sender;
 
 @end
--- ScrollViewSample/ViewController.m
+++ ScrollViewSample/ViewController.m
@@ -13,15 +13,75 @@
 @end
 
 @implementation ViewController
+@synthesize myscrollView;
+@synthesize myField;
+@synthesize myLabel;
+@synthesize myLabel2;
+@synthesize myField2;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+        //追加、scrollViewのサイズを設定
+    self.myscrollView.contentSize = CGSizeMake(320, 460);
+    
+    //以下追加、キーボードが表示されたらスクロール/消えたら元に戻るときにはNotificationを使う
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardShow:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardHide:) name:UIKeyboardDidHideNotification object:nil];
+    keyboardShown = NO;
 }
+
+//以下追加、UIKeyboardDidShowNotificationが送信されたときに実行される
+- (void)keyboardShow:(NSNotification *)notificatioin {
+    
+    if (keyboardShown) {
+        return;
+    }
+    
+    NSDictionary *info = [notificatioin userInfo];
+    
+    //キーボードのサイズを取得
+    NSValue *keyboardValue = [info objectForKey:UIKeyboardFrameEndUserInfoKey];
+    CGSize keySize = [keyboardValue CGRectValue].size;
+    
+    offset = myscrollView.contentOffset;
+    
+    //scrollViewのサイズを変更
+    CGRect scrollViewFrame = myscrollView.frame;
+    scrollViewFrame.size.height -= keySize.height;
+    myscrollView.frame = scrollViewFrame;
+    
+    //スクロールする
+    CGRect textFieldRect = myField.frame;
+    textFieldRect.origin.y += 10;
+    [myscrollView scrollRectToVisible:textFieldRect animated:YES];
+    
+    keyboardShown = YES;
+}
+
+//以下追加、UIKeyboardDidHideNotificationが送信されたときに実行される
+- (void)keyboardHide:(NSNotification *)notificatioin {
+    if (!keyboardShown) {
+        return;
+    }    
+    
+    //scrollViewをもとに戻す
+    myscrollView.frame = CGRectMake(0, 0, 320, 460);
+    myscrollView.contentOffset = offset;
+    keyboardShown = NO;
+    
+}
+
+
 
 - (void)viewDidUnload
 {
+    [self setMyscrollView:nil];
+    [self setMyField:nil];
+    [self setMyLabel:nil];
+    [self setMyLabel2:nil];
+    [self setMyField2:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,4 +91,13 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (IBAction)keyHide:(id)sender {
+    myLabel.text = myField.text;
+ 
+}
+
+- (IBAction)keyHide2:(id)sender {
+    myLabel2.text = myField2.text;
+   
+}
 @end
